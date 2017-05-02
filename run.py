16446e0b43858f88b21d9334a158c2382656b422
Thomas Koehler <basta.t.k+git@gmail.com>
2017-05-02 10:32:14 +0200

    Revive the hostfile!

--- run.py
+++ run.py
@@ -7,6 +7,10 @@ input = '"4k//4K//4P w"'
 if len(sys.argv) >= 3:
     input = sys.argv[2]
 
+hostfile = ""
+if os.path.exists("hostfile"):
+    hostfile = "--hostfile ../hostfile --map-by node "
+
 cmd = "cd seq && make && ./decide {}"
 if len(sys.argv) >= 2:
     if (sys.argv[1] == 'seq'):
@@ -15,7 +19,7 @@ if len(sys.argv) >= 2:
         n = 4
         if len(sys.argv) >= 4:
             n = int(sys.argv[3])
-        cmd = "cd {} && make && mpirun -n {} decide {}".format(sys.argv[1], n, "{}")
+        cmd = "cd {} && make && mpirun -n {} {} decide {}".format(sys.argv[1], n, hostfile, "{}")
     elif (sys.argv[1][0:3] == 'omp'):
         cmd = "cd {} && make && ./decide {}".format(sys.argv[1], "{}")
     else:
